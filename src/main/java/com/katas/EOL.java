1446e2e47d13709f774f848e189c1a7d3bb3de46
i-katas <feexonj@gmail.com>
2020-02-17 21:10:05 +0800

    Introduce temporary variable `pos`

--- src/main/java/com/katas/EOL.java
+++ src/main/java/com/katas/EOL.java
@@ -2,14 +2,19 @@ package com.katas;
 
 public class EOL {
   public static String trim(String lines) {
-    int n = lines.length();
-    int i = lines.indexOf('\n');
+    int pos = 0, n = lines.length();
+    StringBuilder out = new StringBuilder(n);
+    final int i = lines.indexOf('\n', pos);
     if(i != -1) {
       n = i - 1;
     }
     while(n > 0 && lines.charAt(n - 1) == ' ') {
       n--;
     }
-    return lines.substring(0, n) + (i != -1 ? '\n' : "");
+    out.append(lines.substring(pos, n));
+    if(i != -1) {
+      out.append('\n');
+    }
+    return out.toString();
   }
 }
